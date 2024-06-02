d263df7b0a2d4a5294243fd6d71f9e190b4d86f9
ochom <ochom@police.com>
2024-06-02 14:17:30 +0300

    did template tags

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Article
 
 
 # Create your views here.
-def article_list(request):
-    return render(request, 'articles/article_list.html')    
 
+
+def article_list(request):
+    articles = Article.objects.all().order_by('date')       #they aare ordered by date here i.e the latest the first
+    return render(request,'articles/article_list.html', {'articles':articles})      #we have created a dictionary that we are going to send to the template
